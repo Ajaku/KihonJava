36f88c97cce2e8d62c2c9d6d97152008701dd201
Ajaku <Furry.love774@hotmail.co.jp>
2016-11-30 16:39:34 +0900

    Add files via upload

---
+++ kadai2/src/kadai2/kadai2.java
@@ -0,0 +1,25 @@
+package kadai2;
+
+public class kadai2 {
+
+	public static void main(String[] args) {
+
+		int val = 10;
+		int a;
+
+		for (a = 0; a < val; a++) {
+
+			if (a % 2 == 1) {
+
+				System.out.printf("[%d]Odd!\n", a);
+
+			} else {
+
+				System.out.printf("[%d]Even!\n", a);
+
+			}
+		}
+
+	}
+
+}
